dae30d5db9fc441f370ffe9253f1f481e3c98003
jonathaneslava <jadrian1112@gmail.com>
2025-02-27 23:05:52 -0600

    Se agregaron botones de incremento y decremento en extras

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -24,3 +24,19 @@ function checkLimit() {
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', checkLimit);  // Llamar a la función cada vez que cambie el estado de una casilla
 });
+
+let numero = 0; // Valor inicial
+
+function incrementar() {
+    if (numero < 3) { // Limita el incremento hasta 3
+        numero++;
+        document.getElementById("numero").textContent = numero; // Actualiza el número en la página
+    }
+}
+
+function decrementar() {
+    if (numero > 0) { // Limita el decremento hasta 0
+        numero--;
+        document.getElementById("numero").textContent = numero; // Actualiza el número en la página
+    }
+}
